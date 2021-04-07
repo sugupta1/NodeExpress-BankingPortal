cb9f32fe2b3576482600bb989f6891fc5cbc5b9f
sugupta1 <getsubodhgupta@gmail.com>
2021-04-07 19:09:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ const userData = fs.readFileSync(path.join(__dirname,'json','users.json'),'utf8'
 const users = JSON.parse(userData);
 
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Account summary' , accounts});
-});
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
 
 app.get('/savings', (req, res) => {
     res.render('account',{account: accounts.savings});
