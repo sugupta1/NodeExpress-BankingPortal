f2f1f79360e6dd61c2c40859bff9bdb07b163120
sugupta1 <getsubodhgupta@gmail.com>
2021-04-07 15:52:06 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000
-const accountData = fs.readFileSync(src/json/accounts.json,{encoding: 'utf8'});
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(src/json/users.json,{encoding: 'utf8'});
+const userData = fs.readFileSync('src/json/users.json','utf8');
 const users = JSON.parse(userData);
 
 
@@ -20,6 +20,7 @@ app.get('/', (req, res) => {
     res.locals = {title: 'account summary',accounts: accounts};
     res.render('index',{title: 'Index'});
 });
+
 app.get('/savings', (req, res) => {
     res.render('account',{account: accounts.savings});
 });
