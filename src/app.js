467b97edf17685fa3719a40540b3d04b948fca5e
sugupta1 <getsubodhgupta@gmail.com>
2021-04-07 12:13:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000
+const accountData = fs.readFileSync(src/json/accounts.json,{encoding: 'utf8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(src/json/users.json,{encoding: 'utf8'});
+const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index',{title: 'Index'});
