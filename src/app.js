b2be01a2a887d031334cd0088331e95742b709e3
sugupta1 <getsubodhgupta@gmail.com>
2021-04-07 18:09:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ const users = JSON.parse(userData);
 
 
 app.get('/', (req, res) => {
-    res.render('index',{title: 'Index'});
-    res.render('index', { title: 'Account summary', accounts});
+    res.render('index', { title: 'Account summary', accounts: accounts});
 });
 
 app.get('/savings', (req, res) => {
