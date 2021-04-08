5d4858bf220710f6258aa2dc471fa261fcd2d769
sugupta1 <getsubodhgupta@gmail.com>
2021-04-08 16:44:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,18 +19,6 @@ const port = 3000;
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
 
-app.get('/savings', (req, res) => {
-    res.render('account',{account: accounts.savings});
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account',{account: accounts.checking});
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account',{account: accounts.credit});
-});
-
 
 app.get('/profile', (req, res) => {
     res.render('profile',{user: users[0]});
