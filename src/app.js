e8d2fc69ad15a3f837c157c2098e2d8edf05a951
sugupta1 <getsubodhgupta@gmail.com>
2021-04-08 12:35:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,21 +46,24 @@ app.get('/transfer', (req, res) => {
     res.render('transfer');
 });
 
-app.post('/transfer',(req,res) =>{
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + 
-    parseInt(req.body.amount , 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON,'utf8');
-    res.render('transfer',{ message: 'Transfer completed'});
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});
 });
 
 app.get('/payment', (req, res) =>{
     res.render('payment' , {account: accounts.credit });
 });
 
-app.post('/payment', (req,res) =>{
-
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 app.listen(port, () => {
   console.log('PS Project Running on port 3000!')
