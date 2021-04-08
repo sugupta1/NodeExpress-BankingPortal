3a80a58c2fd4ee98cb567290fc30654bbed89ce7
sugupta1 <getsubodhgupta@gmail.com>
2021-04-08 15:45:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -41,8 +41,7 @@ app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance -= req.body.amount;
     accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    // let accountsJSON = JSON.stringify(accounts, null, 4)
-    // fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', {message: 'Transfer Completed'});
 });
 
@@ -53,8 +52,7 @@ app.get('/payment', (req, res) =>{
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount);
-    // let accountsJSON = JSON.stringify(accounts, null, 4)
-    // fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 app.listen(port, () => {
